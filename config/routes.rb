5f975cdfbbc100bbc3c78a6436b561c6b3d89cbb
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-01 15:22:43 +0000

    [Finish] homeアイコンを押すとNameError

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
-  root to: "homes#top"
-  get "home/about" => "homes#about", as: 'about'
+  get 'home/about' => 'homes#about', as: 'about'
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books
+
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
