c5379b37c459530006bb175a4441e96c10c81ebe
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-02 01:54:30 +0000

    [Add]新規会員登録できない（book must exist）

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:show,:edit,:update]
   resources :books
 
-  root to: 'homes#top'
+  root  'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
