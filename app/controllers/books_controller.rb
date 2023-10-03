1c0b6f9ff26374b2747caf0695161f731ed6369c
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-03 09:27:40 +0000

    [Add] 修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,30 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
- before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
-  def show
+ def show
     @book = Book.find(params[:id])
     @book_new = Book.new
-  end
+ end
 
-  def index
+ def index
     @books = Book.all
     @book = Book.new
-  end
+ end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @books = Book.all
-    @user = @book.user
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      flash[:notice] = "Book not create successfully."
-      render "index"
+      render 'index'
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
     @user = User.find(params[:id])
   end
 
@@ -42,21 +40,20 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "successfully delete book!"
+
   end
 
-
- private
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
-
   def ensure_correct_user
     @book = Book.find(params[:id])
-    unless @book.user == current_user
-      redirect_to books_path
-    end
+      unless @book.user == current_user
+        redirect_to books_path
+      end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,15 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user),notice = "You have updated user successfully."
+      redirect_to user_path(@user),notice: "You have updated user successfully."
     else
       render "edit"
     end
@@ -34,9 +34,7 @@ class UsersController < ApplicationController
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to user_path(current_user), alert: "You are not authorized to perform this action."
+      redirect_to user_path(current_user)
     end
-  end
-
   end
 end
