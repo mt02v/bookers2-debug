0b23f00883efcc0482e0d7783bea94245519ecda
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-03 17:19:29 +0000

    [Add]修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users,only: [:show,:index,:edit,:update]
   resources :book
-  root to:  'homes#top'
+  root to:  "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
-
-  
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
