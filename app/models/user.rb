dccfdc926514f9c52c3533e8312b66ce8f3213ca
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-04 07:01:00 +0000

    [Finish]課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   validates :name,  length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  validates :name, uniqueness: true
+  
 
  def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
