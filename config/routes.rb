2e9216c4357598aab2314cb41b3ce47f865c206d
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-07 16:04:05 +0000

    [Add] 修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root to:  "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-  resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:index,:show,:edit,:update] do
