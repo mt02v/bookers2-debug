c6475b3380336f188071f8e290a0194c8971f04a
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-17 06:27:59 +0000

    [Add} 課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,14 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  #scope :スコープ名,　-->　｛ 条件式 ｝
+  #スコープ名で条件式を呼び出せる
+  #where(条件)でほしいデータを取り出す
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
+
 def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
